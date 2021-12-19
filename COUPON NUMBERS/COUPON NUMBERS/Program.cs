2c7e6f638fc059fe9ed973fe8dbd69e8d3d9ea3d
shaikmohammed238 <shaikmohammedghoue238@gmail.com>
2021-12-19 12:01:40 +0530

    added

---
+++ COUPON NUMBERS/COUPON NUMBERS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace COUPON_NUMBERS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Perfect Number/Perfect Number/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Perfect_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter number");
+            int num = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 1; i < num; i++)
+            {
+                if ((num % i) == 0)
+                {
+                    sum = sum + i;
+                    Console.Write(" +" + sum);
+                }
+            }
+            Console.WriteLine("");
+            if (num == sum)
+            {
+                Console.WriteLine("the number is perfect number: " + num);
+            }
+
+        }
+    }
+}
